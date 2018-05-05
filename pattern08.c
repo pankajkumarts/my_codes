2e6c85f25a8d06556bd4dc5472d7bbf48263289d
PANKAJ KUMAR TS <pankaj.sai32@gmail.com>
2018-05-05 15:23:53 +0530

    Create pattern08.c

---
+++ pattern08.c
@@ -0,0 +1,25 @@
+/*
+ 
+ *
+ **
+ ***
+ ****
+ *****
+ 
+ */
+ 
+ #include <stdio.h>
+
+int main() {
+	//code
+	int n;
+    int  i,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+	    for(j=1;j<=i;j++){
+	        printf("*");
+	    }
+	    printf("\n");
+	}
+	return 0;
+}
