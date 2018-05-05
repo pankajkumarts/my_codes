d8610549420a7fd8fa4c52ff47ae21845ad3db83
PANKAJ KUMAR TS <pankaj.sai32@gmail.com>
2018-05-05 14:07:53 +0530

    Create pattern01.c

---
+++ pattern01.c
@@ -0,0 +1,23 @@
+/*
+ ****
+ ****
+ ****
+ ****
+*/
+
+//code
+
+#include <stdio.h>
+
+int main() {
+	//code
+	int n=0,i=0,j=0;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+	    for(j=1;j<=n;j++){
+	        printf("*");
+	    }
+	    printf("\n");
+	}
+	return 0;
+}
