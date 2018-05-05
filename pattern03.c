2601bde15ceff4f04a0f98f2321dd616fe071a34
PANKAJ KUMAR TS <pankaj.sai32@gmail.com>
2018-05-05 14:11:35 +0530

    Create pattern03.c

---
+++ pattern03.c
@@ -0,0 +1,22 @@
+/*
+ 1234
+ 1234
+ 1234
+ 1234
+*/
+//code
+
+#include <stdio.h>
+
+int main() {
+	//code
+	int n=0,i=0,j=0;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+	    for(j=1;j<=n;j++){
+	        printf("%d",j);
+	    }
+	    printf("\n");
+	}
+	return 0;
+}
