3d71e6ac4dcd94d8fec1e37a734408b4248285e5
PANKAJ KUMAR TS <pankaj.sai32@gmail.com>
2018-05-05 15:27:38 +0530

    Create pattern10.c

---
+++ pattern10.c
@@ -0,0 +1,22 @@
+/*
+ 1
+ 12
+ 123
+ 1234
+ 12345
+ */
+ #include <stdio.h>
+
+int main() {
+	//code
+	int n;
+    int  i,j;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+	    for(j=1;j<=i;j++){
+	        printf("%d",j);
+	    }
+	    printf("\n");
+	}
+	return 0;
+}
